5a8ec45635ca7f330ca384a4b2a6c11237972022
setoyama sota <soutlook0103@gmail.com>
2023-03-15 15:51:23 +0900

    プロジェクト新規作成

---
+++ src/main/java/com/sawara/MentaSpringBoot/MentaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sawara.MentaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sawara/MentaSpringBoot/MentaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawara.MentaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
